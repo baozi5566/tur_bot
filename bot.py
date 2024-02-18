17206d27dec53a0fa390092872f618409367507b
Han <pig662930@gmail.com>
2024-02-18 19:23:16 +0800

    setting/pic
    設定檔
    圖片上傳練習

--- bot.py
+++ bot.py
@@ -1,23 +1,43 @@
 import discord
 from discord.ext import commands
+import json 
+import random
+
+with open('setting.json', 'r', encoding="utf8") as jfile:
+    jdata = json.load(jfile)
 
 intents = discord.Intents.default()
 intents.members = True
-
+intents.message_content = True
 bot = commands.Bot(command_prefix= '[', intents = intents)
 
 @bot.event
 async def on_ready():
     print(">> Bot is online <<")
 
+@bot.command()
+async def 圖片(ctx):
+    random_pic = random.choice(jdata['pic'])
+    pic = discord.File(random_pic)
+    await ctx.send(file= pic)
+
+@bot.command()
+async def web圖片(ctx):
+    random_pic = random.choice(jdata['url_pic'])
+    await ctx.send(random_pic)
+
 @bot.event
 async def on_member_join(member):
-    channel = bot.get_channel(1208697980467814470)
+    channel = bot.get_channel(int(jdata['Welcome_channel']))
     await channel.send(f'{member} join!')
 
 @bot.event
 async def on_member_remove(member):
-    channel = bot.get_channel(1208698019763982387)
+    channel = bot.get_channel(int(jdata['Leave_channel']))
     await channel.send(f'{member} leave!')
 
-bot.run("MTIwODY3MzEwNTM4NDU3NDk4Ng.GR1Dzg.6dpH2VBwV7y8h7AwmM-rOsL8WqeMOUVimF4t7Q")
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'{bot.latency*1000}(ms)')
+
+bot.run(jdata['TOKEN'])
