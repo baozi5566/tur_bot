80c623ee06c4a7c118b873b4a0219b63198a4148
Han <pig662930@gmail.com>
2024-02-19 12:03:04 +0800

    cog
    修復ping指令

--- cmds/main.py
+++ cmds/main.py
@@ -2,14 +2,13 @@ import discord
 from discord.ext import commands
 from core.classes import Cog_Extension
 
-class Main(Cog_Extension):
-        
-                
-        @commands.command()
-        async def ping(self, ctx):
-            await ctx.send(f'{self.bot.latency*1000}(ms)')
+class basicComs(Cog_Extension):   
+    
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send(f"{round(self.bot.latency*1000)}(ms)")
             
         
 
 async def setup(bot):
-    await bot.add_cog(Main(bot))
+    await bot.add_cog(basicComs(bot))
